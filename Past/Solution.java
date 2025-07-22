479acf5469aaa05de03b3f76ef9ed97ba06c4b8d
lishuo94 <lishuo94@126.com>
2025-07-22 02:52:04 +0000

    add Past.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Past/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+public class Solution {
+    /**
+     * a message receiver
+     * @param arr int整型一维数组 the array
+     * @return int整型二维数组
+     */
+    public int[][] aMessageReceiver (int[] arr) {
+        // write code here
+        LinkNode myLink=new LinkNode();
+        LinkNode e=new LinkNode();
+        e.value=2;
+        
+
+
+
+
+    }
+    class LinkNode{
+        int value;
+        LinkNode prev;
+        LinkNode next;
+
+
+    }
+
+
+
+
+}
