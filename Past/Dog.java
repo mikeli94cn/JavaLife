bf88b5d1a1d38c0ce8b17fb99577d68569528fa3
lishuo94 <lishuo94@126.com>
2025-07-22 02:42:54 +0000

    add Past.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Past/Dog.java
@@ -0,0 +1,11 @@
+public class Dog{
+    String name;
+    double height;
+    void bark(){
+        System.out.println("wow,wow!");
+    }
+    void eat(){
+        System.out.println("eat food");
+    }
+
+}
