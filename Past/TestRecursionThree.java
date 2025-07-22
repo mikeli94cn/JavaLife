563a57d6c0131bca09e6a8821a342bb8fd27a114
lishuo94 <lishuo94@126.com>
2025-07-22 02:54:29 +0000

    add Past.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Past/TestRecursionThree.java
@@ -0,0 +1,12 @@
+class TestRecursionThree
+{
+    public static void main(String[] args) {
+        System.out.println(f(10));
+    }
+    static int f(int n){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(n==3) return 4;
+        return f(n-1)+f(n-2)+f(n-3);
+    }
+}
