33adb1edb2beb4bcc5cf576416ce2a9e514ab4fd
lishuo94 <lishuo94@126.com>
2025-07-24 12:03:32 +0000

    add Java_Programming_Language/Chapter1/Point.java.
    Signed-off-by: lishuo94 <lishuo94@126.com>

---
+++ Java_Programming_Language/Chapter1/Point.java
@@ -0,0 +1,22 @@
+package javalife.javapl.chapter1;
+
+public class Point {
+    public double x;
+    public double y;
+    public static Point origin=new Point();
+
+    public void clear(){
+        x=0;
+        y=0;
+    }
+    public double distance(Point that){
+        double xdiff=x-that.x;
+        double ydiff=y-that.y;
+        return Math.sqrt(xdiff*xdiff+ydiff*ydiff);
+    }
+
+    public void move(double x,double y){
+        this.x = x;
+        this.y = y;
+    }
+}
