842c6868d0d9404b8a1105e7db57f36697898f99
lishuo94 <lishuo94@126.com>
2026-01-22 06:01:27 +0800

    20260122

--- ocf/TestClass.java
+++ ocf/TestClass.java
@@ -2,21 +2,9 @@ import java.util.*;
 
 class TestClass{
 	public static void main(String[] args){
-		double ran1=Math.random();
-		System.out.println(ran1);
-		
-		String uuid=UUID.randomUUID().toString();
-		System.out.println(uuid);
-
-		Random rand=new Random();
-		
-		System.out.println(Integer.MIN_VALUE);
-		System.out.println(Integer.MAX_VALUE);
-		System.out.println(rand.nextInt());
-		System.out.println(rand.nextInt(100));
-		System.out.println(rand.nextLong());
-
-		System.out.println(rand.nextDouble());
-		System.out.println(rand.nextGaussian());
+		List s1=new ArrayList();
+		s1.add("a");
+		//boolean rs=s1.remove("a");
+		System.out.println(s1.remove("a"));
 	}
 }
