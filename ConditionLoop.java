809c319dc091079f113f72babc5f78bed3b1fefb
mikeli94cn <lishuo94@126.com>
2026-03-27 05:39:39 +0800

    ConditionLoop

---
+++ ConditionLoop.java
@@ -0,0 +1,13 @@
+class ConditionLoop {
+  public static void main(String[] args) {
+    String today = "Friday";
+    String mood;
+    // if-else
+    if (today == "Friday") {
+      mood = "happy";
+    } else {
+      mood = "bad";
+    }
+    System.out.printf("today is %s,mood is %s\n", today, mood);
+  }
+}
